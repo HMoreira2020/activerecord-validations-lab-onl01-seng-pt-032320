3d88d8926a783799e3e364efaf628991d1c8ab82
HMoreira2020 <moreiraheather@gmail.com>
2020-07-23 18:52:10 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+        message: "%{value} is not a valid category"}
+    validate :title_must_be_click_baity 
+
+    def title_must_be_click_baity
+        titles = ["Won't Believe", "Secret", "Top", "Guess"]
+        if title.present? && titles.none?{|t| title.include?(t)}
+          errors.add(:title, "Title must include 'Won't Believe', 'Secret', 'Top', or 'Guess'!")
+        end
+    end
+
+     
+    
 end
